827eaac963e6c9352c7a13a29cf41eca92315550
Gary Yeap <contact@garyyeap.com>
2025-08-09 17:52:57 +0800

    Remove `twIdiom` since its unnecessary for lyrics

--- LyricsX/Component/ChineseConverter+Singleton.swift
+++ LyricsX/Component/ChineseConverter+Singleton.swift
@@ -22,7 +22,7 @@ extension ChineseConverter {
         switch change.newValue {
         case 1: ChineseConverter._shared = try? ChineseConverter(options: [.simplify])
         case 2: ChineseConverter._shared = try? ChineseConverter(options: [.traditionalize])
-        case 3: ChineseConverter._shared = try? ChineseConverter(options: [.traditionalize, .twStandard, .twIdiom])
+        case 3: ChineseConverter._shared = try? ChineseConverter(options: [.traditionalize, .twStandard])
         case 4: ChineseConverter._shared = try? ChineseConverter(options: [.traditionalize, .hkStandard])
         case 0, _: ChineseConverter._shared = nil
         }
