014470022a4853c03a21ab108155086c1162f9da
Mx-Iris <mxiris@126.com>
2026-05-16 20:06:24 +0800

    Revert "fix: anchor lyric scheduling on published PlaybackState; poll past last line"
    This reverts commit 4b6bf4350e2ba99d11289aa3185feb25bda4e3b7.

--- LyricsX/Utility/Extension.swift
+++ LyricsX/Utility/Extension.swift
@@ -26,23 +26,6 @@ extension MusicPlayerName {
     }
 }
 
-extension PlaybackState {
-    static var lyricsRepeatWrapGracePeriod: TimeInterval { 0.5 }
-
-    func lyricsDisplayTime(trackDuration: TimeInterval?) -> TimeInterval {
-        let playbackTime = time
-        guard isPlaying,
-              playbackTime.isFinite,
-              let trackDuration = trackDuration,
-              trackDuration.isFinite,
-              trackDuration > 0,
-              playbackTime >= trackDuration + Self.lyricsRepeatWrapGracePeriod else {
-            return playbackTime
-        }
-        return playbackTime.truncatingRemainder(dividingBy: trackDuration)
-    }
-}
-
 extension MusicTrack {
     var lyrics: String? {
         guard let originalTrack = originalTrack,
